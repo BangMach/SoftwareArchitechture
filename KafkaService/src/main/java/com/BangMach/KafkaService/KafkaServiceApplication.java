e468127e28af6371f2b5fcd78ea24d4e2a5afad0
TamRMIT <s3747274@rmit.edu.vn>
2020-12-29 18:42:35 +0700

    [Kafka] Implement Eureka Client

--- KafkaService/src/main/java/com/BangMach/KafkaService/KafkaServiceApplication.java
+++ KafkaService/src/main/java/com/BangMach/KafkaService/KafkaServiceApplication.java
@@ -2,8 +2,11 @@ package com.BangMach.KafkaService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@EnableEurekaClient
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class KafkaServiceApplication {
 
 	public static void main(String[] args) {
--- RestaurantMenuService/src/main/java/com/BangMach/RestaurantMenuService/DAO/DishDAOImpl.java
+++ RestaurantMenuService/src/main/java/com/BangMach/RestaurantMenuService/DAO/DishDAOImpl.java
@@ -5,8 +5,6 @@ import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 import javax.persistence.EntityManager;
 import java.util.List;
 
