df0c13e5dd5dbcb1d025135dcabca95ce44262be
tienngthth <s3757934@rmit.edu.vn>
2021-01-06 14:28:16 +0700

    [Kafka] Dockerize Kafka Service

--- RestaurantUserService/src/test/java/com/BangMach/RestaurantUserService/RestaurantUserServiceApplicationTests.java
+++ RestaurantUserService/src/test/java/com/BangMach/RestaurantUserService/RestaurantUserServiceApplicationTests.java
@@ -7,9 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RestaurantUserServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantUserServiceApplicationTests.class, args);
 	}
