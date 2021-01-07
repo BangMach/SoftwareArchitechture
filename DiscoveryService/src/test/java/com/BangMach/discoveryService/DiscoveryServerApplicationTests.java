61eba41d295a771a91dacc14b202e81ec70384cd
Tien Nguyen Thi Thuy <48781015+tienngthth@users.noreply.github.com>
2021-01-07 17:29:57 +0700

    Update DiscoveryServerApplicationTests.java

--- DiscoveryService/src/test/java/com/BangMach/discoveryService/DiscoveryServerApplicationTests.java
+++ DiscoveryService/src/test/java/com/BangMach/discoveryService/DiscoveryServerApplicationTests.java
@@ -3,7 +3,7 @@ package com.BangMach.discoveryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 class DiscoveryServerApplicationTests {
 
 	@Test
