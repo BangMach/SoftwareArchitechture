c59890fbc56a15f49e835b40aeaac95029528465
tienngthth <s3757934@rmit.edu.vn>
2021-01-07 17:31:45 +0700

    [AWS] fix build

--- DiscoveryService/src/test/java/com/BangMach/discoveryService/DiscoveryServerApplicationTests.java
+++ DiscoveryService/src/test/java/com/BangMach/discoveryService/DiscoveryServerApplicationTests.java
@@ -2,8 +2,9 @@ package com.BangMach.discoveryService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import javax.ws.rs.core.Application;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 class DiscoveryServerApplicationTests {
 
 	@Test
